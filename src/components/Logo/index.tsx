7fd8667af795a8f8a642e92653ba85050ace02c7
Junior Alves <jjalves96@gmail.com>
2021-06-06 10:45:01 -0300

    feat: Create Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,14 @@
+import { GrSpotify } from 'react-icons/gr';
+
+import * as S from './styles';
+
+const Logo = () => {
+  return (
+    <S.Container>
+      <GrSpotify size="40" />
+      <h1>Spotify</h1>
+    </S.Container>
+  );
+};
+
+export default Logo;
