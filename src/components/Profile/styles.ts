75ca636b3b90ff0e4b23c714ccb4c8fc99e7916f
Junior Alves <jjalves96@gmail.com>
2021-06-06 20:14:57 -0300

    style: Add new styles on component

--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -15,6 +15,12 @@ export const Content = styled.div`
     align-items: center;
     color: ${theme.colors.white};
     font-weight: ${theme.font.bold};
+    transition: color 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      color: ${theme.colors.green};
+    }
 
     p {
       margin-left: ${theme.spacings.xsmall};
